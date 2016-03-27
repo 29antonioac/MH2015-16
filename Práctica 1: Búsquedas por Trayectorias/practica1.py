18d39b3c3b11c85d328d62c49b343c8b9e0fd4ee
Antonio Álvarez <analca3@gmail.com>
2016-03-27 01:00:48 +0100

    Empezando con NumPy!

--- Práctica 1: Búsquedas por Trayectorias/practica1.py
+++ Práctica 1: Búsquedas por Trayectorias/practica1.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
 import numpy as np
+from scipy.io import arff
 from sklearn import datasets
 from sklearn import neighbors
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.cross_validation import check_cv
+from sklearn.preprocessing import MinMaxScaler
 
-iris = datasets.load_iris()
+dataset, metadata = arff.loadarff("Datos/wdbc.arff")
+
+data = dataset[metadata.names()[:-1]]   # Everything but the last column
+data = np.asarray(data.tolist(), dtype=np.float32) # Fix the np.void type
+
+target = dataset[:]["class"]            # Only the class column
+target = np.asarray(target.tolist(), dtype=np.int16)
+
 knn = neighbors.KNeighborsClassifier(n_neighbors = 3, n_jobs = -1)
-knn.fit(iris.data, iris.target)
-prediction = knn.predict([[0.1, 0.2, 0.3, 0.4]])
 
-print(prediction)
+knn.fit(iris.data, iris.target)
