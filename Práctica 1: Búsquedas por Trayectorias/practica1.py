0f94ec96d28c0ad8ad4a6bc372eefacdf583aed8
Antonio Álvarez <analca3@gmail.com>
2016-03-27 01:38:51 +0100

    Añadiendo logging

--- Práctica 1: Búsquedas por Trayectorias/practica1.py
+++ Práctica 1: Búsquedas por Trayectorias/practica1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+import logging
 import numpy as np
 from scipy.io import arff
 from sklearn import datasets
@@ -8,14 +10,37 @@ from sklearn.cross_validation import StratifiedKFold
 from sklearn.cross_validation import check_cv
 from sklearn.preprocessing import MinMaxScaler
 
-dataset, metadata = arff.loadarff("Datos/wdbc.arff")
+def main():
+    logging.basicConfig(level=logging.INFO)
 
-data = dataset[metadata.names()[:-1]]   # Everything but the last column
-data = np.asarray(data.tolist(), dtype=np.float32) # Fix the np.void type
+    logger = logging.getLogger(__name__)
 
-target = dataset[:]["class"]            # Only the class column
-target = np.asarray(target.tolist(), dtype=np.int16)
+    dataset, metadata = arff.loadarff("Datos/wdbc.arff")
+    # dataset, metadata = arff.loadarff("Datos/movement_libras.arff")
+    # dataset, metadata = arff.loadarff("Datos/arrhythmia.arff")
 
-knn = neighbors.KNeighborsClassifier(n_neighbors = 3, n_jobs = -1)
+    data = dataset[metadata.names()[:-1]]   # Everything but the last column
+    data = np.asarray(data.tolist(), dtype=np.float32) # Fix the np.void type
+    m = MinMaxScaler()
+    data = m.fit_transform(data)            # Normalize data
 
-knn.fit(iris.data, iris.target)
+    target = dataset[:]["class"]            # Only the class column
+    target = np.asarray(target.tolist(), dtype=np.int16)
+
+    knn = neighbors.KNeighborsClassifier(n_neighbors = 3, n_jobs = -1)
+
+    skf = StratifiedKFold(target, n_folds=2, shuffle=True)
+
+    for train_index, test_index in skf:
+       logger.debug("TRAIN: " + str(train_index) + " TEST: " + str(test_index))
+       data_train, data_test = data[train_index], data[test_index]
+       target_train, target_test = target[train_index], target[test_index]
+
+       logger.debug("X_train: " + str(data_train) + " X_test: " + str(data_test))
+       logger.debug("y_train: " + str(target_train) + " y_test: " + str(target_test))
+       knn.fit(data_train, target_train)
+       logger.info(knn.score(data_test,target_test))
+    # knn.fit(iris.data, iris.target)
+
+if __name__ == "__main__":
+    main()
