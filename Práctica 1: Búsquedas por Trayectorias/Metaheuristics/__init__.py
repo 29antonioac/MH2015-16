e9e0a52949dd4cabffd745f6f373b3ba49033c00
Antonio Álvarez <analca3@gmail.com>
2016-03-27 03:50:31 +0200

    Intentando que funcione algo!

---
+++ Práctica 1: Búsquedas por Trayectorias/Metaheuristics/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import numpy as np
+from sklearn import neighbors
+
+def SFS(data_train, data_test, target_train, target_test, feature_names, classifier):
+    selected_features = np.zeros(len(feature_names), dtype=np.bool)
+    best_score = 0
+    best_feature = 0
+
+    # selected_features[0] = True
+    # print(data_train[:,selected_features])
+    print(target_train)
+
+    while best_feature is not None:
+        end = True
+        best_feature = None
+
+        for idx, feature in enumerate(feature_names):
+            if selected_features[idx]:
+                continue
+
+            selected_features[idx] = True
+            classifier.fit(data_train[:,selected_features], target_train)
+            score = classifier.score(data_test, target_test)
+            selected_features[idx] = False
+
+            if score > best_score:
+                best_score = score
+                best_feature = idx
+
+        if best_feature is not None:
+            selected_features[best_feature] = True
+
+    return selected_features, best_score
--- Práctica 1: Búsquedas por Trayectorias/practica1.py
+++ Práctica 1: Búsquedas por Trayectorias/practica1.py
@@ -4,23 +4,25 @@ import sys
 import logging
 import numpy as np
 from scipy.io import arff
-from sklearn import datasets
 from sklearn import neighbors
 from sklearn.cross_validation import StratifiedKFold
-from sklearn.cross_validation import check_cv
 from sklearn.preprocessing import MinMaxScaler
+
+from Metaheuristics import *
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
     logger = logging.getLogger(__name__)
 
-    dataset, metadata = arff.loadarff("Datos/wdbc.arff")
-    # dataset, metadata = arff.loadarff("Datos/movement_libras.arff")
-    # dataset, metadata = arff.loadarff("Datos/arrhythmia.arff")
+    dataset, metadata = arff.loadarff("Data/wdbc.arff")
+    # dataset, metadata = arff.loadarff("Data/movement_libras.arff")
+    # dataset, metadata = arff.loadarff("Data/arrhythmia.arff")
 
-    data = dataset[metadata.names()[:-1]]   # Everything but the last column
+    feature_names = metadata.names()[:-1]  # Everything but the last column
+    data = dataset[feature_names]
     data = np.asarray(data.tolist(), dtype=np.float32) # Fix the np.void type
+
     m = MinMaxScaler()
     data = m.fit_transform(data)            # Normalize data
 
@@ -32,13 +34,11 @@ def main():
     skf = StratifiedKFold(target, n_folds=2, shuffle=True)
 
     for train_index, test_index in skf:
-       logger.debug("TRAIN: " + str(train_index) + " TEST: " + str(test_index))
        data_train, data_test = data[train_index], data[test_index]
        target_train, target_test = target[train_index], target[test_index]
+       selected_features, score = SFS(data_train, data_test, target_train, target_test, feature_names, knn)
 
-       logger.debug("X_train: " + str(data_train) + " X_test: " + str(data_test))
-       logger.debug("y_train: " + str(target_train) + " y_test: " + str(target_test))
-       knn.fit(data_train, target_train)
+
        logger.info(knn.score(data_test,target_test))
     # knn.fit(iris.data, iris.target)
 
