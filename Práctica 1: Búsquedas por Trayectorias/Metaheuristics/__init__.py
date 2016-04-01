77e9052702be85e6cb3398f5d100f536554df8af
Antonio Álvarez <analca3@gmail.com>
2016-04-02 01:56:15 +0200

    Añadida TS y extendida

--- Práctica 1: Búsquedas por Trayectorias/Metaheuristics/__init__.py
+++ Práctica 1: Búsquedas por Trayectorias/Metaheuristics/__init__.py
@@ -64,7 +64,7 @@ def LS(data_train, target_train, classifier):
     end = False
 
     best_score = score_solution(data_train, target_train, selected_features, scores, classifier)
-    evaluations = 1
+    evaluations = 0
     while not end and evaluations < MAX_EVALUATIONS:
         l_neighbors = list(enumerate(selected_features))
         np.random.shuffle(l_neighbors)
@@ -104,7 +104,7 @@ def SA(data_train, target_train, classifier):
 
     T0 = mu * best_score / (-np.log(phi))
     Tf = 1e-3
-    
+
     max_neighbors = 10 * rowsize
     max_accepted = 0.1 * max_neighbors
     M = np.ceil(MAX_EVALUATIONS / max_neighbors)
@@ -118,6 +118,12 @@ def SA(data_train, target_train, classifier):
         while evaluations < MAX_EVALUATIONS and actual_neighbors < max_neighbors and accepted_neighbors < max_accepted:
             feature = np.random.randint(rowsize)
             flip(selected_features, feature)
+
+            # Check if there isn't any feature
+            while sum(selected_features) == 0:
+                feature = np.random.randint(rowsize)
+                flip(selected_features, feature)
+
             new_score = score_solution(data_train, target_train, selected_features, scores, classifier)
             flip(selected_features, feature)
             deltaF = new_score - actual_score
@@ -135,3 +141,156 @@ def SA(data_train, target_train, classifier):
         T = T / (1 + beta * T)
 
     return selected_features, best_score
+
+def TS(data_train, target_train, classifier):
+    rowsize = len(data_train[0])
+    data_number = data_train.shape[0]
+
+    initial_sol = np.zeros(rowsize, dtype=np.bool)
+    initial_sol[np.random.randint(2)] = True
+
+    scores = np.zeros(data_number, dtype=np.float32)
+
+    selected_features = initial_sol
+
+    best_score = score_solution(data_train, target_train, selected_features, scores, classifier)
+
+    size_tabu_list = rowsize // 3
+    tabu_list = np.repeat(-1,size_tabu_list)
+
+    pos_tabu_list = 0
+
+    evaluations = 0
+    size_neighbourhood = 30
+
+    while evaluations < MAX_EVALUATIONS:
+        actual_score = 0
+        best_feature = -1
+
+        neighbourhood = np.random.choice(rowsize, size_neighbourhood, replace = False)
+
+        for idx in neighbourhood:
+            flip(selected_features, idx)
+            # Check if there isn't any feature
+            while sum(selected_features) == 0:
+                feature = np.random.randint(rowsize)
+                flip(selected_features, feature)
+
+            new_score = score_solution(data_train, target_train, selected_features, scores, classifier)
+            flip(selected_features, idx)
+
+            evaluations += 1
+
+            if idx in tabu_list:
+                if new_score > best_score and new_score > actual_score:
+                    actual_score = new_score
+                    best_feature = idx
+            elif new_score > actual_score:
+                actual_score = new_score
+                best_feature = idx
+
+            if evaluations >= MAX_EVALUATIONS:
+                break
+
+        pos_tabu_list = (pos_tabu_list + 1) % size_tabu_list
+        tabu_list[pos_tabu_list] = best_feature
+
+        if (actual_score > best_score):
+            best_score = actual_score
+            flip(selected_features, best_feature)
+
+    return selected_features, best_score
+
+def TSext(data_train, target_train, classifier):
+    rowsize = len(data_train[0])
+    data_number = data_train.shape[0]
+
+    initial_sol = np.zeros(rowsize, dtype=np.bool)
+    initial_sol[np.random.randint(2)] = True
+
+    scores = np.zeros(data_number, dtype=np.float32)
+    frec = np.zeros(rowsize, dtype=np.int32)
+
+    selected_features = initial_sol
+
+    best_score = score_solution(data_train, target_train, selected_features, scores, classifier)
+
+    size_tabu_list = rowsize // 3
+    tabu_list = np.repeat(-1,size_tabu_list)
+
+    pos_tabu_list = 0
+
+    evaluations = 0
+    size_neighbourhood = 30
+
+    iterations_without_improve = 0
+
+    best_solution = np.zeros(rowsize, dtype=np.bool)
+
+    while evaluations < MAX_EVALUATIONS:
+        actual_score = 0
+        best_feature = -1
+
+        if iterations_without_improve >= 10:
+            print("Reset!")
+            iterations_without_improve = 0
+            choice = np.random.uniform()
+            if choice < 0.25: # New random solution
+                selected_features = np.random.choice([True, False], rowsize) #initial solution
+            elif choice < 0.5: # New from best solution
+                selected_features = best_solution
+            else: # Use memory
+                total_sols = sum(frec)
+                selected_features = np.array([(np.random.uniform() < 1 - i / total_sols) for i in frec], dtype=np.bool)
+
+            change_size = np.random.uniform()
+            if change_size < 0.5:
+                size_tabu_list *= 1.5
+                size_tabu_list = np.ceil(size_tabu_list)
+                tabu_list.resize(size_tabu_list)
+            else:
+                size_tabu_list /= 1.5
+                size_tabu_list = np.ceil(size_tabu_list)
+                tabu_list.resize(size_tabu_list)
+
+        neighbourhood = np.random.choice(rowsize, size_neighbourhood, replace = False)
+
+        for idx in neighbourhood:
+            flip(selected_features, idx)
+            # Check if there isn't any feature
+            while sum(selected_features) == 0:
+                feature = np.random.randint(rowsize)
+                flip(selected_features, feature)
+
+            new_score = score_solution(data_train, target_train, selected_features, scores, classifier)
+            flip(selected_features, idx)
+
+            evaluations += 1
+
+            if idx in tabu_list:
+                if new_score > best_score and new_score > actual_score:
+                    actual_score = new_score
+                    best_feature = idx
+                    frec[idx] += 1
+                else:
+                    iterations_without_improve += 1
+            elif new_score > actual_score:
+                actual_score = new_score
+                best_feature = idx
+                frec[idx] += 1
+            else:
+                iterations_without_improve += 1
+
+            if evaluations >= MAX_EVALUATIONS:
+                break
+
+
+        pos_tabu_list = (pos_tabu_list + 1) % size_tabu_list
+        tabu_list[pos_tabu_list] = best_feature
+
+        if (actual_score > best_score):
+            best_score = actual_score
+            flip(selected_features, best_feature)
+            best_solution = np.copy(selected_features)
+
+    return best_solution, best_score
