bd9a97f8200c539a8f5f53ee3d56f7c46b310f77
Antonio Álvarez <analca3@gmail.com>
2016-04-01 03:38:11 +0200

    Tablas hechas y revisadas. Hay que quitar basura

--- Práctica 1: Búsquedas por Trayectorias/practica1.py
+++ Práctica 1: Búsquedas por Trayectorias/practica1.py
@@ -22,68 +22,67 @@ def main():
 
     logger = logging.getLogger(__name__)
 
-
-
-    databases = {   "W" : "Data/wdbc.arff",
-                "L" : "Data/movement_libras.arff",
-                "A" : "Data/arrhythmia.arff"}
-
-    dataset, metadata = arff.loadarff("Data/wdbc.arff")
-    # dataset, metadata = arff.loadarff("Data/movement_libras.arff")
-    # dataset, metadata = arff.loadarff("Data/arrhythmia.arff")
-
-    feature_names = metadata.names()[:-1]  # Everything but the last column
-    data = dataset[feature_names]
-    data = np.asarray(data.tolist(), dtype=np.float32) # Fix the np.void type
-
-    m = MinMaxScaler()
-    data = m.fit_transform(data)            # Normalize data
-
-    target = dataset[:]["class"]            # Only the class column
-    target = np.asarray(target.tolist(), dtype=np.int16)
-
     algorithms_table = {}
     algorithms = [LS]
 
-    knn = neighbors.KNeighborsClassifier(n_neighbors = 3, n_jobs = 1)
+    databases = {   "W" : "Data/wdbc.arff",
+                    "M" : "Data/movement_libras.arff",
+                    "A" : "Data/arrhythmia.arff"}
 
-    for iteration in range(1):
-        skf = StratifiedKFold(target, n_folds=2, shuffle=True)
+    for key, value in databases.items():
+        np.random.seed(12345678)
 
-        for run, partition in enumerate(skf):
-            train_index = partition[0]
-            test_index = partition[1]
-            data_train, data_test = data[train_index], data[test_index]
-            target_train, target_test = target[train_index], target[test_index]
+        dataset, metadata = arff.loadarff(value)
 
-            for alg in algorithms:
-                actual_table = algorithms_table.setdefault(alg.__name__, {})
-                actual_items = actual_table.setdefault("items", [])
-                item = {}
+        feature_names = metadata.names()[:-1]  # Everything but the last column
+        data = dataset[feature_names]
+        data = np.asarray(data.tolist(), dtype=np.float32) # Fix the np.void type
 
-                start = time()
-                selected_features, score = alg(data_train, target_train, knn)
-                end = time()
+        m = MinMaxScaler()
+        data = m.fit_transform(data)            # Normalize data
 
-                knn.fit(data_train[:,selected_features], target_train)
-                score_out = knn.score(data_test[:,selected_features], target_test)
+        target = dataset[:]["class"]            # Only the class column
+        target = np.asarray(target.tolist(), dtype=np.int16)
 
-                item["name"] = "Partición " + str(iteration+1) + "-" + str(run+1)
-                item["W_clas_in"] = score
-                item["W_clas_out"] = score_out
-                item["W_red"] = 100*(len(selected_features) - sum(selected_features)) / len(selected_features)
-                item["W_T"] = end-start
+        knn = neighbors.KNeighborsClassifier(n_neighbors = 3, n_jobs = 1)
+        repeats = 1
+        n_folds = 2
 
-                actual_items.append(item)
+        for iteration in range(repeats):
+            print("Iteration",iteration,"/",repeats)
+            skf = StratifiedKFold(target, n_folds=n_folds, shuffle=True)
 
-                print()
+            for run, partition in enumerate(skf):
+                train_index = partition[0]
+                test_index = partition[1]
+                data_train, data_test = data[train_index], data[test_index]
+                target_train, target_test = target[train_index], target[test_index]
 
-                logger.info(str(alg.__name__) + " - Time elapsed: " + str(end-start) + ". Score: " + str(score) + ". Score out: " + str(score_out) + " Selected features: " + str(sum(selected_features)))
+                for alg in algorithms:
+                    actual_table = algorithms_table.setdefault(alg.__name__, {})
+                    actual_items = actual_table.setdefault("items", [{} for _ in range(repeats*n_folds)])
+                    item = actual_items[2*iteration + run]
 
+                    start = time()
+                    selected_features, score = alg(data_train, target_train, knn)
+                    end = time()
+
+                    knn.fit(data_train[:,selected_features], target_train)
+                    score_out = knn.score(data_test[:,selected_features], target_test)
+
+                    item["name"] = "Partición " + str(iteration+1) + "-" + str(run+1)
+                    item[key+"_clas_in"] = score
+                    item[key+"_clas_out"] = score_out
+                    item[key+"_red"] = 100*(len(selected_features) - sum(selected_features)) / len(selected_features)
+                    item[key+"_T"] = end-start
+
+                    logger.info(key + " - " + str(alg.__name__) + " - Time elapsed: " + str(end-start) + ". Score: " + str(score) + ". Score out: " + str(score_out) + " Selected features: " + str(sum(selected_features)))
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
     print("Volcando tablas...")
-    print(algorithms_table)
+    pp.pprint(algorithms_table)
     print("-----------------")
-    print(algorithms_table["LS"])
+    pp.pprint(algorithms_table["LS"])
     with open("Other/template.mu", "r") as template:
         for alg in algorithms:
             with open("Other/"+alg.__name__+"-result.mu", "w") as dest:
